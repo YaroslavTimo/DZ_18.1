75604c86dcf43f031509517d03ae354e19afe478
YaroslavTimo <kotnarkot11m@gmail.com>
2023-10-17 16:37:41 +0300

    struct Emloyee

---
+++ DZ_18.1/Employeeh.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+struct Employee {
+    string lastName;
+    int birthYear;
+    string position;
+    double salary;
+    string education;
+};
